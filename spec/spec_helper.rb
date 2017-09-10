607a8d23d2353dd27600589c02dca3d48ed6b40e
Bastian Schwarz <bastian@codename-php.de>
2017-09-10 14:47:12 +0200

    Added tests and figured out how rspec works

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,10 @@
 require 'chefspec'
 require 'chefspec/berkshelf'
+
+RSpec.configure do |config|
+  # Specify the operating platform to mock Ohai data from (default: nil)
+  config.platform = 'debian'
+
+  # Specify the operating version to mock Ohai data from (default: nil)
+  config.version = '9.1'
+end
--- spec/unit/recipes/openjdk8_spec.rb
+++ spec/unit/recipes/openjdk8_spec.rb
@@ -9,7 +9,7 @@ require 'spec_helper'
 describe 'chef.cookbook.dev::openjdk8' do
   context 'When all attributes are default, on an unspecified platform' do
     let(:chef_run) do
-      runner = ChefSpec::ServerRunner.new
+      runner = ChefSpec::SoloRunner.new
       runner.converge(described_recipe)
     end
 
