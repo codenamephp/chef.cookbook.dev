4f4d8dc664e4dcffb863cf8e65c944ffab246731
Bastian Schwarz <bastian@codename-php.de>
2018-08-17 22:39:31 +0200

    Fixes #9 Removed openjdk8 recipe

--- recipes/openjdk8.rb
+++
@@ -1,9 +0,0 @@
-package 'Install openjdk8' do
-  package_name 'openjdk-8-jdk'
-  notifies :run, 'execute[Set java alternatives]', :immediately
-end
-
-execute 'Set java alternatives' do
-  action :nothing
-  command 'sudo update-java-alternatives --set java-1.8.0-openjdk-amd64'
-end
