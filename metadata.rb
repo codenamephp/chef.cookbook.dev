9b5204133231055e75434251d76c8129d9395973
Vagrant Default User <vagrant@buster.localdomain>
2019-12-05 09:21:31 +0000

    Bumped version for release

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ maintainer_email 'bastian@codename-php.de'
 license 'Apache-2.0'
 description 'Installs all dev tools for infomax project development like browsers, git, openjdk, ...'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '2.1.0'
+version '2.1.1'
 chef_version '>= 13.0' if respond_to?(:chef_version)
 issues_url 'https://github.com/codenamephp/chef.cookbook.dev/issues' if respond_to?(:issues_url)
 source_url 'https://github.com/codenamephp/chef.cookbook.dev' if respond_to?(:source_url)
