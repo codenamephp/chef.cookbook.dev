9b31dbf4de181b2b5b027672d15ac800ba605c26
Bastian Schwarz <bastian@codename-php.de>
2020-06-20 19:01:25 +0200

    Added git workflow and updated travis build

--- metadata.rb
+++ metadata.rb
@@ -7,11 +7,11 @@ license 'Apache-2.0'
 description 'Installs all dev tools for infomax project development like browsers, git, openjdk, ...'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '2.3.0'
-chef_version '>= 13.0' if respond_to?(:chef_version)
-issues_url 'https://github.com/codenamephp/chef.cookbook.dev/issues' if respond_to?(:issues_url)
-source_url 'https://github.com/codenamephp/chef.cookbook.dev' if respond_to?(:source_url)
+chef_version '>= 13.0'
+issues_url 'https://github.com/codenamephp/chef.cookbook.dev/issues'
+source_url 'https://github.com/codenamephp/chef.cookbook.dev'
 
-supports 'debian', '~>10.0'
+supports 'debian'
 
 depends 'sc_vscode', '~>1.0'
 depends 'resolver', '~>2.0'
