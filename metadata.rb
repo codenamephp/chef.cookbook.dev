d2de004f549433ace4bd2508a4c33aec470bc5d8
Bastian Schwarz <bastian@codename-php.de>
2021-07-03 15:22:25 +0200

    Added unified_mode to resources and set minimal chef version to 15.3 since this is the first version that supports unified_mode

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ maintainer_email 'bastian@codename-php.de'
 license 'Apache-2.0'
 description 'Installs all dev tools for infomax project development like browsers, git, openjdk, ...'
 version '3.0.1'
-chef_version '>= 13.0'
+chef_version '>= 15.3'
 issues_url 'https://github.com/codenamephp/chef.cookbook.dev/issues'
 source_url 'https://github.com/codenamephp/chef.cookbook.dev'
 
